6a50f29e1ee1d9e607dbe3de1361441f7ddc692e
HighestDev <kenneth.danso@amalitech.org>
2023-05-09 18:38:46 +0000

    adding empty pages to pages so that we dont get an error when we exceeds the paginator length

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 def post_list(request):
     post_list = Post.objects.all()
     paginator=Paginator(post_list,3)
     page_number=request.GET.get('page',1)
-    posts= paginator.page(page_number)
-    context = {"posts": posts}
-    return render(request, 'blog/post/post_list.html', context)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/post_list.html', {'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, publish__year=year,publish__month=month,publish__day=day)
