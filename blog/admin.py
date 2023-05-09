71e0fe4d9d5dec951dadbfbda19fa3e5f44af7a6
HighestDev <kenneth.danso@amalitech.org>
2023-05-09 06:48:25 +0000

    customize post admin file

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['title', 'slug', 'author']
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'body']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
