74add878b7e4de6a516e971e924db5665ccb42ca
HighestDev <kenneth.danso@amalitech.org>
2023-05-10 11:46:16 +0000

    setting up email in settngs.py to send mail

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 def post_list(request):
     post_list = Post.objects.all()
     paginator=Paginator(post_list,3)
@@ -18,3 +19,14 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, publish__year=year,publish__month=month,publish__day=day)
     return render(request, 'blog/post/detail.html', {"post": post})
+
+def post_share(request,post_id):
+    posts=Post.objects.all()
+    if request.method=='POST':
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+        else:
+            form=EmailPostForm()
+    context={"posts":posts,"form":form}
+    return render(request,'email.txt',context)
