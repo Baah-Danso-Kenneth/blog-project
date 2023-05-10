534d5b801ef502cdce5d0688c82edef1e0711f97
HighestDev <kenneth.danso@amalitech.org>
2023-05-10 05:49:10 +0000

    if page is not an integer the user must be redirected to the first page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def post_list(request):
     post_list = Post.objects.all()
     paginator=Paginator(post_list,3)
@@ -9,6 +9,8 @@ def post_list(request):
 
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/post_list.html', {'posts':posts})
